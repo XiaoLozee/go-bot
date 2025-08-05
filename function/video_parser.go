8ffd4d54fb94cb4bead323cd67eb40c6c874ce1d
xiaoluozhi <xiaoluozhitcsn@gmail.com>
2025-08-05 21:17:28 +0800

    将视频最大大小放到配置文件

--- function/video_parser.go
+++ function/video_parser.go
@@ -6,10 +6,12 @@ import (
 	"github.com/XiaoLuozee/go-bot/botapi"
 	"github.com/XiaoLuozee/go-bot/handler"
 	"github.com/XiaoLuozee/go-bot/registry"
+	"github.com/spf13/viper"
 	"io"
 	"net/http"
 	"net/url"
 	"regexp"
+	"strconv"
 )
 
 type APIResponse struct {
@@ -59,7 +61,30 @@ func videoParser(message string, msgType int, id int64) {
 		return
 	}
 
+	viper.SetDefault("video_parser.video_max_size", 50)
+	videoMaxSize := viper.GetInt("video_parser.video_max_size")
+
 	botapi.SendMultiImageWithTextMsg(msgType, id, []string{videoData.CoverURL}, "视频解析成功\n标题:"+videoData.Title+"\n作者:"+videoData.Author.Name+"\n正在发送视频")
+	videoSizeThreshold := int64(videoMaxSize) * 1024 * 1024
+
+	// 获取远程视频的大小
+	fileSize, err := getRemoteFileSize(videoData.VideoURL)
+	if err != nil {
+		fmt.Printf("获取视频大小失败 (%v)，将作为文件发送\n", err)
+		botapi.SendFileMsg(msgType, id, videoData.VideoURL, videoData.Title+".mp4")
+		return
+	}
+
+	// 将字节大小转换为 MB
+	fileSizeMB := float64(fileSize) / (1024 * 1024)
+
+	if fileSize > videoSizeThreshold {
+		fmt.Println("视频超过阈值，将作为文件发送")
+		botapi.SendTextMsg(msgType, id, fmt.Sprintf("视频大小为 %.2fMB，超过%dMB，将以文件形式发送", fileSizeMB, videoMaxSize))
+		botapi.SendFileMsg(msgType, id, videoData.VideoURL, videoData.Title+".mp4") // 文件名可以自定义
+	} else {
+		botapi.SendVideoMsg(msgType, id, videoData.VideoURL)
+	}
 	botapi.SendVideoMsg(msgType, id, videoData.VideoURL)
 }
 
@@ -73,6 +98,38 @@ func regexpMatchUrlFromString(text string) (string, error) {
 	}
 
 	return match, nil
+}
+
+// getRemoteFileSize 获取远程文件的大小
+func getRemoteFileSize(fileURL string) (int64, error) {
+	req, err := http.NewRequest("HEAD", fileURL, nil)
+	if err != nil {
+		return -1, fmt.Errorf("创建 HEAD 请求失败: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return -1, fmt.Errorf("发送 HEAD 请求失败: %w", err)
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return -1, fmt.Errorf("服务器返回非 200 状态码: %s", resp.Status)
+	}
+
+	contentLengthStr := resp.Header.Get("Content-Length")
+	if contentLengthStr == "" {
+		return -1, fmt.Errorf("服务器未在响应头中提供 Content-Length")
+	}
+
+	size, err := strconv.ParseInt(contentLengthStr, 10, 64)
+	if err != nil {
+		return -1, fmt.Errorf("解析 Content-Length 失败: %w", err)
+	}
+
+	return size, nil
 }
 
 // parseVideoShareURL 调用外部 API 来解析视频分享链接。
